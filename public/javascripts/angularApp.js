7717cef6cd3ed92c25f8937b39abb94dbbf6e786
sadmansk <sadmansazidk@gmail.com>
2015-05-13 14:11:35 -0400

    Added main index page file and angular app javascript file

---
+++ public/javascripts/angularApp.js
@@ -0,0 +1,65 @@
+var app = angular.module('flapperNews', ['ui.router']);
+
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: '/home.html',
+        controller:'MainCtrl'
+      })
+
+      .state('posts', {
+	url: '/posts/{id}',
+	templateUrl: '/posts.html',
+	controller: 'PostsCtrl'
+      });
+
+    $urlRouterProvider.otherwise('home');
+}]);
+
+app.factory('posts', [function(){
+    var o = {
+      posts: [{title: 'name', link: 'link', upvotes: '0', comments: []}]
+    };
+    return o;
+}]);
+
+app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts) { $scope.posts = posts.posts;
+    $scope.addPost = function(){
+      if(!$scope.title || $scope.title === '') { return; }
+      $scope.post = ({title: $scope.title,
+	link:$scope.link,
+	upvotes: 0,
+	comments: [
+	  {author: 'Joe', body: 'Cool post!', upvotes: 69}
+	]
+      });
+      $scope.posts.push($scope.post);
+      $scope.title = '';
+      $scope.link = '';
+    };
+
+    $scope.incrementUpvotes = function(post){
+      $scope.post.upvotes += 1;
+    };
+}]);
+
+app.controller('PostsCtrl', ['$scope', '$stateParams', 'posts', function ($scope, $stateParams, posts) {
+    $scope.post = posts.posts[$stateParams.id];
+    $scope.incrementUpvotes = function(){
+      $scope.post.upvotes += 1;
+    };
+
+    $scope.addComment = function() {
+      if ($scope.body === '' || !$scope.body) { return; }
+      $scope.post.comments.push({
+	author: 'user',
+	body: $scope.body,
+	upvotes:0
+      });
+      $scope.body = '';
+    };
+}]);
